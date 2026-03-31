8aa0c9d98163438da5e4ba11470d7384dc3ec7e3
Aldiss1 <aldisuryasaputra693@gmail.com>
2026-03-31 19:39:02 +0700

    JS9 Filament

--- app/Filament/Resources/Products/Schemas/ProductInfolist.php
+++ app/Filament/Resources/Products/Schemas/ProductInfolist.php
@@ -6,6 +6,8 @@ use Filament\Schemas\Components\Section;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Infolists\Components\ImageEntry;
 use Filament\Infolists\Components\IconEntry;
+use Filament\Schemas\Components\Tabs;
+use Filament\Schemas\Components\Tabs\Tab;
 use Filament\Schemas\Schema;
 
 class ProductInfolist
@@ -15,6 +17,71 @@ class ProductInfolist
         return $schema
             ->components([
                 //
+                Tabs::make('Product Tabs')
+                    ->tabs([
+                        Tab::make('Product Details')
+                            ->schema([
+                                TextEntry::make('name')
+                                    ->label('Product Name')
+                                    ->weight('bold')
+                                    ->color('primary'),
+                                TextEntry::make('id')
+                                    ->label('Product ID'),
+                                TextEntry::make('sku')
+                                    ->label('Product SKU')
+                                    ->badge()
+                                    ->color('success'),
+                                TextEntry::make('description')
+                                    ->label('Product Description'),
+                                TextEntry::make('created_at')
+                                    ->label('Product Creation Date')
+                                    ->date('d M Y')
+                                    ->color('info'),
+                            ]),
+                        Tab::make('Product Price and Stock')
+                            ->schema([
+                                TextEntry::make('price')
+                                    ->label('Product Price')
+                                    ->weight('bold')
+                                    ->color('primary')
+                                    ->icon('heroicon-s-currency-dollar'),
+                                TextEntry::make('stock')
+                                    ->label('Product Stock')
+                                    ->badge()
+                                    ->color(fn ($state) => match(true) {
+                                        $state <= 0 => 'danger',
+                                        $state <= 10 => 'warning',
+                                        default => 'success',
+                                    }),
+                            ]),
+                        Tab::make('Image and Status')
+                            ->schema([
+                                ImageEntry::make('image')
+                                    ->label('Product Image')
+                                    ->disk('public'),
+                                TextEntry::make('price')
+                                    ->label('Product Price')
+                                    ->weight('bold')
+                                    ->color('primary')
+                                    ->icon('heroicon-s-currency-dollar'),
+                                TextEntry::make('stock')
+                                    ->label('Product Stock')
+                                    ->weight('bold')
+                                    ->badge()
+                                    ->color(fn ($state) => match(true) {
+                                        $state <= 0 => 'danger',
+                                        $state <= 10 => 'warning',
+                                        default => 'success',
+                                    }),
+                                IconEntry::make('is_active')
+                                    ->label('Is Active?')
+                                    ->boolean(),
+                                IconEntry::make('is_featured')
+                                    ->label('Is Featured?')
+                                    ->boolean(),
+                            ])
+                    ])->columnSpanFull()
+                    ->vertical(),
                 Section::make('Product Info')
                     ->description('')
                     ->schema([
@@ -47,7 +114,13 @@ class ProductInfolist
                             ->formatStateUsing(fn ($state) => 'Rp ' . number_format((float) $state, 0, ',', '.')),
                         TextEntry::make('stock')
                             ->label('Product Stock')
-                            ->icon('heroicon-s-cube'),
+                            ->icon('heroicon-s-cube')
+                            ->badge()
+                            ->color(fn ($state) => match(true) {
+                                $state <= 0 => 'danger',
+                                $state <= 10 => 'warning',
+                                default => 'success',
+                            }),
                     ])
                     ->columnSpanFull(),
                 Section::make('Image and Status')
@@ -65,8 +138,13 @@ class ProductInfolist
                         TextEntry::make('stock')
                             ->label('Product Stock')
                             ->weight('bold')
-                            ->color('primary')
-                            ->icon('heroicon-s-cube'),
+                            ->icon('heroicon-s-cube')
+                            ->badge()
+                            ->color(fn ($state) => match(true) {
+                                $state <= 0 => 'danger',
+                                $state <= 10 => 'warning',
+                                default => 'success',
+                            }),
                         IconEntry::make('is_active')
                             ->label('Is Active?')
                             ->boolean(),
